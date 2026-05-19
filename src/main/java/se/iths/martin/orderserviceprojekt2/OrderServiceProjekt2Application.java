9cd59996ce0a48be7c197cd83c745903a4e9191d
MartinGruter <Martin.Grter@iths.se>
2026-05-19 10:05:22 +0200

    Initial commit

---
+++ src/main/java/se/iths/martin/orderserviceprojekt2/OrderServiceProjekt2Application.java
@@ -0,0 +1,13 @@
+package se.iths.martin.orderserviceprojekt2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderServiceProjekt2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderServiceProjekt2Application.class, args);
+	}
+
+}
---
+++ src/test/java/se/iths/martin/orderserviceprojekt2/OrderServiceProjekt2ApplicationTests.java
@@ -0,0 +1,13 @@
+package se.iths.martin.orderserviceprojekt2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceProjekt2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
