ae17372d6301aecd3de25310c52c4907c58fe5d6
ynsuldg <yns.uldg@hotmail.com>
2026-05-25 23:26:34 +0200

    Annotations added to all 4 DTO klasses.

--- src/main/java/dto/OrderItemResponseDTO.java
+++ src/main/java/dto/OrderItemResponseDTO.java
@@ -1,7 +1,16 @@
 package dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.math.BigDecimal;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class OrderItemResponseDTO {
     private Long productId;
     private String Name;
--- src/main/java/dto/OrderResponseDTO.java
+++ src/main/java/dto/OrderResponseDTO.java
@@ -1,8 +1,17 @@
 package dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.math.BigDecimal;
 import java.util.List;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class OrderResponseDTO {
     private Long id;
     private String customerName;
